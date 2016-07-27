30c565363e174922a7ea7601c83b6ddee12bf96e
BAE SEONGUK <ccvf2@naver.com>
2016-07-27 16:52:25 +0900

    관리자 메인페이지 작성중

--- src/main/java/com/mynetgear/ccvf3/admin/main/AdminMainController.java
+++ src/main/java/com/mynetgear/ccvf3/admin/main/AdminMainController.java
@@ -1,5 +1,32 @@
 package com.mynetgear.ccvf3.admin.main;
 
-public class AdminMainController {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.mynetgear.ccvf3.HomeController;
+import com.mynetgear.ccvf3.test.TestService;
+
+@Controller
+public class AdminMainController {
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@Autowired
+	private TestService testService;
+
+	
+	@RequestMapping(value = "/admin/main.do", method = RequestMethod.GET)
+	public ModelAndView adminMainView() {
+		ModelAndView mav = new ModelAndView();
+		//logger.debug("test Mathod start");
+		//List<TestDTO> testList = testService.test();
+		//mav.addObject("list", testList);
+		mav.setViewName("/admin/main");
+
+		return mav;
+	}
 }
