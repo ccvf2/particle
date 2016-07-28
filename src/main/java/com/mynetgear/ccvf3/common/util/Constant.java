a7ea9685999f0c84ad783556ced3a2093883b496
Seong Gook Kwak <ksg9155@gmail.com>
2016-07-28 16:05:01 +0900

    공통 Util 추가

---
+++ src/main/java/com/mynetgear/ccvf3/common/util/Constant.java
@@ -0,0 +1,71 @@
+package com.mynetgear.ccvf3.common.util;
+
+public class Constant {
+	/** 개발자 로그 : 개발자 전용 */
+	public static final String LOG_ID1 = "★★★log★★★  ";
+	/** 개발자 로그 : Aspect default */
+	public static final String LOG_ID2 = "※userInfo:  ";
+	/** 개발자 로그 : Aspect Error */
+	public static final String LOG_ID3 = "※userError:  ";
+
+	/** 문자 셋트 설정 : UTF-8 */
+	public static final String CHARACTER_SET_UTF8 = "UTF-8";
+
+	/** YN : Y */
+	public static final String SYNB_YN_Y = "Y0001";
+	/** YN : N */
+	public static final String SYNB_YN_N = "Y0002";
+	/** YN : Delete */
+	public static final String SYNB_YN_D = "Y0003";
+
+	/** CRUD : C */
+	public static final String SYNB_CRUD_C = "C";
+	/** CRUD : R */
+	public static final String SYNB_CRUD_R = "R";
+	/** CRUD : U */
+	public static final String SYNB_CRUD_U = "U";
+	/** CRUD : D */
+	public static final String SYNB_CRUD_D = "D";
+
+	/** NULLSTRING : "" */
+	public static final String SYNB_NULL = "";
+	/** NULLSTRING : " " */
+	public static final String SYNB_NULL_SPACE = " ";
+	/** DOTSTRING : "." */
+	public static final String SYNB_DOT = ".";
+	/** COMMASTRING : "," */
+	public static final String SYNB_COMMA = ",";
+
+	/** String Numbr : 1 */
+	public static final String SYNB_NUM_1 = "1";
+	/** String Numbr : 2 */
+	public static final String SYNB_NUM_2 = "2";
+	/** String Numbr : 3 */
+	public static final String SYNB_NUM_3 = "3";
+	/** String Numbr : 4 */
+	public static final String SYNB_NUM_4 = "4";
+	/** String Numbr : 5 */
+	public static final String SYNB_NUM_5 = "5";
+	/** String Numbr : 6 */
+	public static final String SYNB_NUM_6 = "6";
+	/** String Numbr : 7 */
+	public static final String SYNB_NUM_7 = "7";
+	/** String Numbr : 8 */
+	public static final String SYNB_NUM_8 = "8";
+	/** String Numbr : 9 */
+	public static final String SYNB_NUM_9 = "9";
+	/** String Numbr : 0 */
+	public static final String SYNB_NUM_0 = "0";
+	
+	/** 화면으로 메세지 전송 : alert */
+	public static final String ALERT_MSG = "alert_msg";
+	/** 화면으로 메세지 전송 : alert */
+	public static final String RETURN_URL = "return_url";
+	
+
+	/**이동할페이지: 로그인전 시도했던 페이지로 이동*/
+	public static final String CALLBACK_URL = "callback_url";
+	/**이동할페이지: 로그인전 시도했던 페이지로 이동 시 파라메터*/
+	public static final String CALLBACK_PARAM = "callback_param";
+	
+}
