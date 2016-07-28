3cfafa392fd11cb0b2327a9db7fd890160bc0cc3
Seong Gook Kwak <ksg9155@gmail.com>
2016-07-28 18:00:23 +0900

    빠졌던 Annotation 추가

--- src/main/java/com/mynetgear/ccvf3/user/main/MainController.java
+++ src/main/java/com/mynetgear/ccvf3/user/main/MainController.java
@@ -3,12 +3,14 @@ package com.mynetgear.ccvf3.user.main;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.mynetgear.ccvf3.HomeController;
 
+@Controller
 public class MainController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 		
